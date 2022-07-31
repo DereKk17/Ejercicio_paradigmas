d6f85e9d5ac4f66c266de781adafc013e8b4927f
Derek Sarmiento Loeber <dereksarmiento1@gmail.com>
2022-07-31 14:16:28 -0500

    Initial commit

---
+++ src/Ejercicio_Materias.java
@@ -0,0 +1,30 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+public class Ejercicio_Materias {
+    public static void main(String[] args) {
+        String nombre;
+        int cedula;
+        String [] asignaturasDisponibles;
+        String [] asignaturasInscirtas;
+        System.out.println("BIENVENIDO AL AREA DE INCRIPCION DE ASIGNATURAS");
+        System.out.println("dijite el numero deseado para acceder al menu");
+
+        System.out.println("1.revisar asignaturas disponibles");
+        System.out.println("2. inscribir asignaturas");
+        System.out.println("3. Cancelar asinaturas preinscritas");
+        System.out.println("4. Mostrar asignaturas preinscritas");
+        System.out.println("5. liquidar materia");
+
+
+
+        Scanner entrada = new Scanner(System.in);
+            System.out.print("Ingrese su nombre: ");
+            nombre = entrada.next();
+            System.out.print("Ingresa su cedula: ");
+            cedula = entrada.nextInt();
+            System.out.print(cedula);
+
+    }
+}
